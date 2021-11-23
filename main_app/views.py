6978985b618e12e0ffc75a2fa274eee5bbab993e
sforesi <sebastiano.foresi@gmail.com>
2021-11-23 15:02:22 -0500

    create and set up base.html

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello Rider!</h1>')
+
+def about(request):
+  return render(request, 'about.html')
