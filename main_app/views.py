ecbef4eed2ad5c40ffd23e1054ed924e9858ba25
sforesi <sebastiano.foresi@gmail.com>
2021-11-23 14:42:58 -0500

    set up urls.py in main app and clutch_up

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello Rider!</h1>')
