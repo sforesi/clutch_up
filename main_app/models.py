d65823f94c3a3266b664c4f42ff5d6cca62845c1
sforesi <sebastiano.foresi@gmail.com>
2021-11-24 17:27:56 -0500

    create trip model in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
+from typing import Type
 from django.db import models
 
 # Create your models here.
+class Trip(models.Model): 
+  name = models.CharField(max_length=100)
+  # type = models.CharField(max_length=1, default= 's',choices=Type)
+  distance = models.IntegerField()
+  description = models.TextField(max_length=500)
+  topspeed = models.IntegerField()
