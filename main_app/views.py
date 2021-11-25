985a700d1538a12acccd42d946356214e4655509
sforesi <sebastiano.foresi@gmail.com>
2021-11-24 20:46:09 -0500

    create and attach model in db

--- main_app/views.py
+++ main_app/views.py
@@ -18,3 +18,15 @@ def trips_index(request):
 def home(request):
   return render(request, 'home.html')
 
+# class Trip:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, distance, description, topspeed):
+#     self.name = name
+#     self.distance = distance
+#     self.description = description
+#     self.topspeed = topspeed
+
+# trips = [
+#   Trip('Bushwick Cruise', 84, 'Late night, super cold. The bridges looked beautiful though.', 53 ),
+#   Trip('Jersey Motorsports', 213, 'Jersey was a blast, tires were cold at first but got em to temps and the Striple was sticking like glue in the corners.', 202),
+#   Trip('Long Island Woods', 55, 'Trails were nice and clear, no big obstacles, got to feel like a rally driver for the afternoon.', 37),
+# ]
