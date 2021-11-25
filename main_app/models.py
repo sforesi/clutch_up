05639025ce74fd6b5e75ac977ac682d28edaa6ae
sforesi <sebastiano.foresi@gmail.com>
2021-11-24 21:58:54 -0500

    fix print output for types in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,12 @@
 from typing import Type
 from django.db import models
 
-TYPE = (('s', 'Street'),('o', 'Offroad'),('t', 'Track'))
+TYPE = (('Street', 'Street'),('Offroad', 'Offroad'),('Track', 'Track'))
 
 # Create your models here.
 class Trip(models.Model): 
   name = models.CharField(max_length=100)
-  type = models.CharField(max_length=1, default=TYPE[0][0],choices=TYPE)
+  type = models.CharField(max_length=150, default=TYPE[0][0],choices=TYPE)
   distance = models.IntegerField()
   description = models.TextField(max_length=500)
   topspeed = models.IntegerField()
