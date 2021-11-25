08c7428da125f67ace0aa27c9b647de706ac8ae1
sforesi <sebastiano.foresi@gmail.com>
2021-11-24 21:39:41 -0500

    replace a tag in index.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  # route for trip index
   path('trips/', views.trips_index, name='trips_index'),
   path('trips/<int:trip_id>/', views.trips_detail, name='trips_detail'),
 ]
