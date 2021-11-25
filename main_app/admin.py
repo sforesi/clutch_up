d0aa73bb856cc909f312e41ae72466fd672a9c89
sforesi <sebastiano.foresi@gmail.com>
2021-11-24 20:59:24 -0500

    reister Models in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+# import your models here
+from .models import Trip
+
+# Register your models here
+admin.site.register(Trip)
