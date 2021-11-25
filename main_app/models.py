979b3cdaab1ca0690cf66b1c511f5921f91a4c76
sforesi <sebastiano.foresi@gmail.com>
2021-11-24 20:27:41 -0500

    add Trip model

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,14 @@
 from typing import Type
 from django.db import models
 
+TYPE = (('s', 'Street'),('r', 'Road'),('t', 'Track'))
+
 # Create your models here.
 class Trip(models.Model): 
   name = models.CharField(max_length=100)
-  # type = models.CharField(max_length=1, default= 's',choices=Type)
+  type = models.CharField(max_length=1, default=TYPE[0][0],choices=TYPE)
   distance = models.IntegerField()
   description = models.TextField(max_length=500)
   topspeed = models.IntegerField()
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,9 @@
+# IMPORTS
 from django.shortcuts import render
-
-# Add the following import
 from django.http import HttpResponse
+from .models import Trip
 
-# Define the home view
+# VIEW DEFINITIONS
 def home(request):
   return HttpResponse('<h1>Hello Rider!</h1>')
 
@@ -12,20 +12,9 @@ def about(request):
 
 # Add new view
 def trips_index(request):
+  trips = Trip.objects.all()
   return render(request, 'trips/index.html', { 'trips': trips })
 
 def home(request):
   return render(request, 'home.html')
 
-class Trip:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, distance, description, topspeed):
-    self.name = name
-    self.distance = distance
-    self.description = description
-    self.topspeed = topspeed
-
-trips = [
-  Trip('Bushwick Cruise', 84, 'Late night, super cold. The bridges looked beautiful though.', 53 ),
-  Trip('Jersey Motorsports', 213, 'Jersey was a blast, tires were cold at first but got em to temps and the Striple was sticking like glue in the corners.', 202),
-  Trip('Long Island Woods', 55, 'Trails were nice and clear, no big obstacles, got to feel like a rally driver for the afternoon.', 37),
-]
