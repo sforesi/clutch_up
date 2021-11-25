69527c80755a481606e80800143701cad11408b9
sforesi <sebastiano.foresi@gmail.com>
2021-11-24 22:09:48 -0500

    create add a trip ui and import CreateView in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 # IMPORTS
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 from .models import Trip
 
 # VIEW DEFINITIONS
