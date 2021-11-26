0cb9951ba252621c520a668d74d86cd0608bf90c
sforesi <sebastiano.foresi@gmail.com>
2021-11-26 11:33:04 -0500

    login working

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('trips/', views.trips_index, name='trips_index'),
   path('trips/<int:trip_id>/', views.trips_detail, name='trips_detail'),
