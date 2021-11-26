0dcaab20b1f44772a2c25c9ba724b6b64ffbdd6d
sforesi <sebastiano.foresi@gmail.com>
2021-11-26 14:49:57 -0500

    add signup view function in views.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,7 @@
 # IMPORTS
 from django.urls import path
 from . import views
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+
 
 
 urlpatterns = [
--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,8 @@ from .models import Trip
 # Add UdpateView & DeleteView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # VIEW DEFINITIONS
 class Home(LoginView):
@@ -24,6 +26,25 @@ def home(request):
 def trips_detail(request, trip_id):
   trip = Trip.objects.get(id=trip_id)
   return render(request, 'trips/detail.html', { 'trip': trip })
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('cats_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
 
 class TripCreate(CreateView):
   model = Trip
