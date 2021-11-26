4021074d78012b65d3cca347616472483127ca02
sforesi <sebastiano.foresi@gmail.com>
2021-11-26 16:50:44 -0500

    iimport redirect to views.py sign up working

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 # IMPORTS
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Trip
 # Add UdpateView & DeleteView
